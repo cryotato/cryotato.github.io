b42fc73c9971fc86bfa9ab8958234aa6f3c5dbc0
Ryan Leo <106399331+cryotato@users.noreply.github.com>
2025-05-04 19:46:31 +0100

    Update marquee.js

--- assets/js/marquee.js
+++ assets/js/marquee.js
@@ -6,12 +6,10 @@ document.addEventListener('DOMContentLoaded', function() {
   // Ensure both elements exist before proceeding
   if (marqueeText1 && marqueeText2) {
     const marqueeMessages = [
-      "Welcome to the cryotato site!", // Example message 1
-      "Check out the latest portfolio items!", // Example message 2
-      "Don't forget to subscribe to the mailing list!", // Example message 3
-      "This text is randomly selected!", // Example message 4
-      "Have a look around the shop maybe?" // Example message 5
-      // Add more messages here if you like
+      "I saw a savior, a savior come my way, I thought I'd see it at the cold light of day, But now I realize that I'm only for me. If only I could see you turn myself to me, And recognize the poison in my heart. There is no other place, no one else I face, No remedy will agree with how I feel. Here in my reflecting, what more can I say? For I am guilty for the voice that I obey, Too scared to sacrifice a choice chosen for me. If only I could see you turn myself to me, And recognize the poison in my heart. There is no other place, no one else I face. No remedy to agree with how I feel.",
+      "POWER IN MISERY TRAVERSING THE GRID OF DEATH",
+      "MACHINE GUN MACHINE FUN!!",
+      "NO!! RAT!!! YOU DON'T HAVE TO DO THIS!"
     ];
 
     // Select a random message
